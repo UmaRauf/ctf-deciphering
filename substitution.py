6a0e56858d10783c3e78ad1a0fc10013bef98aa0
umar <umar110302@gmail>
2024-10-23 04:18:35 +0100

    frequency analysis

---
+++ substitution.py
@@ -0,0 +1,44 @@
+# Define your ciphertext
+ciphertext = """nydsyt,fkhlp22,1845.eairfhqhlrti:ayjhrerenrhswrypiqfnpryq"swreftftiswrplyt,"uwrhrswrplytbyekpfltriswfswrdwyjpitysnrdyeldhrkhrdrtsri"uwrtswrplytduhysrwldsyha."lfempfiswrslerwfdbyeruwrtswr"plytduhlsrwldsyha."urwfgrnrrtprqspytmrtyjmwsymfswrhswrbwfhfbsrhyqdpfgrhaqhyeswrltgypjtsfharglirtbryqswrefdsrhd.ytrelmws,ltirri,hrdsdjqqlblrtspadfsldqlriulswuwfs,lsldrglirts,ejdsnr,ltmrtrhfp,swrhrdjpsdyqdjbwfhrpfslyt,ulswyjsdrrzltmqfhswrhsyqltiuwrswrhswrawfgrqyppyuriltrgrhaltdsftbr.ltirri,swydruwydsfhrfsswrwfpq-krbzyqbyhtfurrz,ftipygrsybyjtsswrpfdwrdytswrdpfgr'dnfbz,fhrdrpiyeswr"dsjqq"yjsyquwlbwhrqyherhdftifnyplslytldsdfhrsynrefir.lhrerenrhswfs,lt1838,eftaurhruflsltmqyhswrhrdjpsdyqswrurdsltilfrvkrhlerts,nrqyhrswrabyjpibyerltsyyjhhftzd.swydr"hrdjpsd"wfgrbyerpytmfmy;njs,fpfd!qruyqswfstjenrhwfgrbyerulswswre,fdbytgrhsd.feftejdsnrildkydrisyojimryqreftblkfslytnayswrhsrdsdswftuwrswrhlswfdltbhrfdriswrkhyijbryqdjmfh,--ftisywfsrdpfgrhaqyhyswrhhrfdytdswftnrbfjdrlsdsfhgrdertftiuwlkduyert,--nrqyhrwrldhrfiasypfaswrqlhdsdsytryqwldftsl-dpfgrhaplqr.lufdmpfisyprfht,ltayjhdsyha,wyurfhpaswreydstrmprbsriyqmyi'dbwlpihrtufzrtsyfdrtdryqswrlhhlmwsd,ftiyqswrltojdslbriytrswre.rvkrhlrtbrldfzrrtsrfbwrh;ftipytmnrqyhrayjwfiefdsrhriayjhfnb,yhztruuwrhrswr"uwlsrdflpd"yqswrbwrdfkrfzrurhrnyjti,ayjnrmft,ldrr,symfjmrswruhrsbwritrddyqswrdpfgr,tysnawldwjtmrhftiufts,tysnawldpfdwrdftisylp,njsnaswrbhjrpftinplmwsltmirfswuwlbwmfswrhdygrhwlddyjp.ltbyttrbslytulswswld,swrhrldytrblhbjedsftbruwlbwefzrdayjhhrbypprbslytdkrbjplfhpagfpjfnpr,ftihrtirhdayjhrfhpaltdlmwsswreyhrhrefhzfnpr.ayjbyerqhyeswfskfhsyqswrbyjtshauwrhrurfhrsypidpfgrhafkkrfhdulswlsdqflhrdsqrfsjhrd.prsjdwrfh,swrt,uwfslsldfslsdnrdsrdsfsr--mfxrytlsdnhlmwsdlir,lqlswfdytr;ftiswrtlefmltfslytefasfdzwrhkyurhdsyfiiifhzpltrdsyswrklbsjhr,fddwrshfgrpddyjswufhisyswfs(qyhswrbypyhrieft)gfpprayqswrdwfiyuyqirfsw,uwrhrswrelddlddlkkldurrkdfpytm.fmflt,urwfgrztyutayjpytm,ftibftkjsswreydsrtslhrbytqlirtbrltayjhshjsw,bftiyh,ftidltbrhlsa.rgrhaytruwywfdwrfhiayjdkrfzwfdqrps,fti,lfebytqlirts,rgrhaytruwyhrfidayjhnyyzulppqrrp,krhdjfiriswfsayjmlgrswrefqflhdkrblertyqswruwyprshjsw.tyytr-dlirikyhshfls,--tyuwyprdfprbyekpfltsd,--njsdshlbsojdslbriytr,uwrtrgrhltilglijfpzltipltrddwfdtrjshfplxri,qyhfeyerts,swrirfipadadsreulswuwlbwlsufddshftmrpafpplri.ayjwfgrnrrtulswjd,syy,dyerarfhd,ftibftqflhpabyekfhrswrsulplmwsyqhlmwsd,uwlbwayjhhfbrrtoyafsswrtyhsw,ulswswfs"tyytyqtlmws"jtirhuwlbwswrapfnyhdyjswyqefdytftiilvyt'dpltr.srppjduwrswrh,fqsrhfpp,swrwfpq-qhrrbypyhrieftyqefddfbwjdrssdlduyhdryqqswftswrkfekrhridpfgryqswrhlbrdufekd!lthrfiltmayjhplqr,tyytrbftdfaswfsurwfgrjtqflhpaklbzriyjsdyerhfhrdkrblertdyqbhjrpsa.urztyuswfsswrnlssrhihykd,uwlbwrgrtayjwfgrihfltriqhyeswrbjk,fhrtyltblirtsfpfmmhfgfslytd,tyltilglijfplppd,njsdjbwfdejdseltmprfpufadftitrbrddfhlpaltswrpysyqrgrhadpfgr.swrafhrswrrddrtslfpltmhrilrtsd,tysswrybbfdlytfphrdjpsd,yqswrdadsre.fqsrhfpp,ldwfpphrfiayjhnyyzulswshrenpltmqyhayj.dyerarfhdfmy,uwrtayjurhrnrmlttltmsysrpperayjhhrfptferftinlhswkpfbr,ayjefahrerenrhldsykkriayj,ftikhrqrhhrisyhrefltlmtyhftsyqfpp.ulswswrrvbrkslytyqfgfmjrirdbhlkslyt,dylbytsltjri,slppswryswrhifa,uwrtayjhrfierayjhereylhd.lwfhipaztru,fsswrsler,uwrswrhsyswftzayjyhtysqyhswrdlmwsyqswre,uwrtlhrqprbsriswfslsufddslppiftmrhyjd,ltefddfbwjdrssd,qyhwytrdsertsysrppswrlhtferd!swradfaswrqfswrhd,lt1776,dlmtriswrirbpfhfslytyqltirkrtirtbrulswswrwfpsrhfnyjsswrlhtrbzd.ayj,syy,kjnpldwayjhirbpfhfslytyqqhrriyeulswiftmrhbyekfddltmayjfhyjti.ltfppswrnhyfipftiduwlbwswrbytdslsjslytyqswrjtlsridsfsrdygrhdwfiyud,swrhrldtydltmprdkys,--wyurgrhtfhhyuyhirdypfsr,--uwrhrfqjmlslgrdpfgrbftkpftswledrpqftidfa,"lfedfqr."swruwyprfheyhayqtyhswrhtpfuwfdtydwlrpiqyhayj.lfeqhrrsydfaswfs,ltayjhkpfbr,ldwyjpiswhyuswred.ltsyswrqlhr.ayj,krhwfkd,efasrppayjhdsyhaltdfqrsa,rtirfhrifdayjfhrsydyeftaufhewrfhsdnahfhrmlqsd,ftifdslpphfhrhirgyslytyqswresyswrdrhglbryqyswrhd.njslsulppnryultmytpasyayjhpfnyhd,ftiswrqrfhprddrqqyhsdyqswydruwy,shfekpltmswrpfudftibytdslsjslytyqswrbyjtshajtirhswrlhqrrs,fhrirsrheltriswfsswraulpp"wlirswryjsbfds,"ftiswfsswrlhwrfhswddwfppnr,dklsryqswrpfu,ftfdapjeqyhswrykkhrddri,lq,dyersleryhyswrh,swrwjenprdsefadsftiltyjhdshrrsd,ftinrfhulstrddltdfqrsafmfltdsswrbhjrpslrdyquwlbwwrwfdnrrtswrglbsle.arslslddfisyswltz,swfsswrdrgrhaswhynnltmwrfhsduwlbwurpbyerayjhdsyha,ftiqyheayjhnrdsdfqrmjfhiltsrppltmls,fhrfppnrfsltmbytshfhasyswr"dsfsjsrltdjbwbfdrefirftikhygliri."myyt,eairfhqhlrti,slppayj,ftiswydruwy,plzrayj,wfgrnrrtdfgri,dyfdnaqlhr,qhyeswrifhzkhldyt-wyjdr,dwfppdsrhrysakrswrdrqhrr,lpprmfpkjpdrdltsydsfsjsrd;ftitrurtmpfti,bjssltmpyydrqhyefnpyyi-dsfltrijtlyt,dwfppmpyhaltnrltmswrwyjdryqhrqjmrqyhswrykkhrddri,--slppurtypytmrherhrpa"_wlir_swryjsbfds,"yhefzrferhlsyqdsftiltmlipanauwlprwrldwjtsriltyjhelids;njs,bytdrbhfsltmftruswrdylpyqswrklpmhledfdftfdapjeqyhswrykkhrddri,khybpfleyjh_urpbyer_syswrdpfgrdypyjipa,swfsswrsytrddwfpphrfbwrgrhawjsltswrbfhypltfd,ftiefzrswrnhyzrt-wrfhsrinytieftprfkjkfsswrswyjmwsyqypiefddfbwjdrssd.myidkrriswrifa!_slppswrt,ftirgrh,_ayjhdshjpa,urtirppkwlpplkdkdltbfdrswldprssrhldrtbhaksri,lltbpjirswrdsftifhikwytrslbfpkwfnrs,syrtdjhrswfsswrzraefsbwltmldbyhhrbs:fpqf,nhfgy,bwfhplr,irpsf,rbwy,qyvshys,mypq,wysrp,ltilf,ojplrss,zlpy,plef,elzr,tygrenrh,ydbfh,kfkf,cjrnrb,hyery,dlrhhf,sftmy,jtlqyhe,glbsyh,uwldzra,v-hfa,aftzrr,xjpj.swrqpfmayjtrrisydjnelsldqpfm{tphruigcszvftedh}"""
+
+# Define your substitution mapping
+substitution_map = {
+    'a': 'y',
+    'b': 'c',
+    'c': 'q',
+    'd': 's',
+    'e': 'm',
+    'f': 'a',
+    'g': 'v',
+    'h': 'r',
+    'i': 'd',
+    'j': 'u',
+    'k': 'p',
+    'l': 'i',
+    'm': 'g',
+    'n': 'b',
+    'o': 'j',
+    'p': 'l',
+    'q': 'f',
+    'r': 'e',
+    's': 't',
+    't': 'n',
+    'u': 'w',
+    'v': 'x',
+    'w': 'h',
+    'x': 'z',
+    'y': 'o',
+    'z': 'k'
+}
+
+
+def substitute(ciphertext, mapping):
+    substituted_text = []
+    for char in ciphertext:
+        substituted_char = mapping.get(char, char)
+        substituted_text.append(substituted_char)
+    return ''.join(substituted_text)
+
+# Call the substitution function and print the result
+result = substitute(ciphertext, substitution_map)
+print(result)
